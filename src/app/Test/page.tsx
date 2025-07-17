86a710217c703bcf3889e189d5ea326ab886b039
SahilMaheshwari19 <sahil19singhi@gmail.com>
2025-07-18 00:14:04 +0530

    setup repo with nextjs and shadcn with test page

---
+++ src/app/Test/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ModeToggle } from "./ModeToggle";
+
+function page() {
+  return (
+    <>
+      <div>page</div>
+      <ModeToggle />
+    </>
+  );
+}
+
+export default page;
